5fb2281e53f6a8e1527189f9f2af71fadd48725b
Suphan Khaomoon <jack231029@gmail.com>
2021-03-17 19:13:25 +0700

    1st commit

---
+++ StringExam.java
@@ -0,0 +1,16 @@
+package CTF01.Java.part2.No6;
+
+public class StringExam
+{
+
+	public static void main(String[] args)
+	{
+		
+//		สั่งสร้าง String จาก char อะไรก็ได้
+		char[] myChar = {'S', 'u', 'p', 'h', 'a', 'n', ' ', 'K', 'h', 'a', 'o', 'm', 'o', 'o', 'n'};
+		String myWord = new String (myChar);
+		System.out.println(myWord);
+
+	}
+
+}
